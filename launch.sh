ben16w/minui-portmaster

commit ec090d4f7d4c2431f00a412a0d76dc2a624c94f9

launch.sh: move gamedir patching to runtime (#136)

launch.sh
@@ -451,17 +451,31 @@ main() {
         done
 
         show_message "Applying changes, please wait..." &
-        shell_scripts=$(find_shell_scripts "$ROM_DIR")
-        echo "Updating shebangs for game scripts..."
-        echo "$shell_scripts" | update_shebangs_from_list
-        echo "Updating PortMaster path for game scripts..."
-        echo "$shell_scripts" | filter_files_with_string "/roms/ports/PortMaster" | update_portmaster_path_from_list
         replace_progressor_binaries "$PORTS_DIR"
         copy_artwork
         copy_game_scripts
         process_squashfs_files "$EMU_DIR/libs"
     else
         echo "Starting PortMaster with port: $ROM_PATH"
+
+        directory="${TEMP_DATA_DIR#/}"
+        gamedir_line=$(grep '^GAMEDIR=' "$ROM_PATH")
+        eval "$gamedir_line"
+        echo "Game dir is: $GAMEDIR"
+
+        if [ -n "$GAMEDIR" ]; then
+            shell_scripts=$(find_shell_scripts "$GAMEDIR")
+            echo "Updating shebangs for game scripts..."
+            echo "$shell_scripts" | update_shebangs_from_list
+            echo "Updating PortMaster path for game scripts..."
+            echo "$shell_scripts" | filter_files_with_string "/roms/ports/PortMaster" | update_portmaster_path_from_list
+        else
+            # If we can't find the GAMEDIR to patch let's not attempt to
+            # run it, since running an unpatched Port will power down NextUI.
+            echo "No GAMEDIR found in $ROM_PATH, not executing game."
+            show_message "Patch failed, exiting..." 3
+            exit 1
+        fi
 
         nintendo_file=$(find "$USERDATA_PATH/PORTS-portmaster" -maxdepth 1 -iname "nintendo*" -type f)
         if [ -n "$nintendo_file" ]; then

commit 7798cd4bd3c0a16a88cc0519b7115dad87612304

fix: patch shell scripts before execution
An oversight in ec090d4f7d caused us to miss patching shell scripts,
causing all games to be unable to load. This resolves that by patching
the file directly before running it.

launch.sh
@@ -460,6 +460,14 @@ main() {
     else
         echo "Starting PortMaster with port: $ROM_PATH"
 
+        # Patch the shell script before executing the game
+        echo "Updating shebang for $ROM_PATH..."
+        update_file_shebang "$ROM_PATH"
+        echo "Updating PortMaster path for $ROM_PATH..."
+        if grep -q "/roms/ports/PortMaster" "$ROM_PATH"; then
+            echo "$ROM_PATH" | update_portmaster_path_from_list
+        fi
+
         directory="${TEMP_DATA_DIR#/}"
         gamedir_line=$(grep '^GAMEDIR=' "$ROM_PATH")
         eval "$gamedir_line"